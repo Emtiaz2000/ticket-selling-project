ea415af5d739ccf43d2f4b40121e00eb0dca6cd2
Emtiaz2000 <62500633+Emtiaz2000@users.noreply.github.com>
2024-02-22 21:09:09 +0600

    Add files via upload

---
+++ ticket-main/app.js
@@ -0,0 +1,43 @@
+//all ticket button
+let ticketBtns = document.querySelectorAll('.seatPosition')
+//ticket showing table
+let ticketInfoTable = document.querySelector('.ticake_info_table')
+//total amount display
+let totalAmoutDisplay = document.querySelector('.totalAmount')
+//grand total 
+let grandTotalDisplay = document.querySelector('.grandTotal')
+//ticket price
+let ticketprice = 550;
+//initial total ticket price
+let totalAmount=0;
+
+//clicking all ticket btn
+ticketBtns.forEach((ticketBtn)=>{
+    ticketBtn.addEventListener('click',()=>{
+        //getting seat number
+        let seatName = ticketBtn.textContent
+        //adding price to current amount
+        totalAmount=totalAmount+ticketprice;
+        //adding booked ticket to table
+        bookingTicket(seatName,ticketprice)
+        //changing the total amount on table
+        totalAmoutDisplay.textContent=totalAmount;
+        //changing the grand total
+        grandTotalDisplay.textContent=totalAmount;
+    })
+})
+
+//booking ticket
+function bookingTicket(seatName,price){
+    //creating tr
+    let tr = document.createElement('tr')
+    //adding table class list
+    tr.className='table bg-transparent'
+    //data in td
+    tr.innerHTML=`<td class="bg-transparent">${seatName}</td>
+    <td class="bg-transparent">Economoy</td>
+    <td class="bg-transparent">${price}</td>`
+    //adding tr-data to table
+    ticketInfoTable.appendChild(tr)
+    
+}
